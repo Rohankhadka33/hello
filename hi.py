e09666fbe26b2fea7a7d664c1d77ff800de8f2e7
Dell <rohankhadka33@gmail.com>
2021-02-16 09:27:11 +0545

    intrest

---
+++ hi.py
@@ -0,0 +1,6 @@
+#
+principle= int(input('Enter the value of principle'))
+time= int(input('Enter the value of time'))
+rate= int(input('Enter the value of rate'))
+intrest= (principle*time*rate)/100
+print('intrest=',intrest)
